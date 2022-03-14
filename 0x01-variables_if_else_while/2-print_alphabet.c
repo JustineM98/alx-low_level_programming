e25f776583b1ef70920cc2c415f297b15aec64a0
JustineM98 <justinemugishe98@gmail.com>
2022-03-15 01:50:33 +0200

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,14 +5,23 @@
  * followed by a new line
  * Return: Always 0 (Success) 
  */
+
 int main(void)
+
 {
+
 char ch;
 for (ch = 'a';
 ch <= 'z'; ch++)
+
 {	
+
 putchar(ch);
+
 }
+
 putchar('\n');
+
 return (0);
+
 }
