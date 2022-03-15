9ca04645ccc0a8294655c5e1833cf96a637eb81e
JustineM98 <justinemugishe98@gmail.com>
2022-03-15 18:35:37 +0200

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,35 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
+/* Declaration of print_putchar function */
+void print_putchar(void);
 
 /**
- * main - print_putchar
- * Return: 0 if existed properly
+ * main - Entry poin
+ *
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	putchar('_putchar');
-	return(0);
+	print_putchar();
+	return (0);
+}
 
+/**
+ * print_putchar - write _putchar string to the stdout
+ */
+void print_putchar(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+"0-putchar.c" 33L, 440C
+/* prototype of _putchar function */
+int _putchar(char c);
+
+/* prototype of print_putchar function */
+void print_putchar(void);
