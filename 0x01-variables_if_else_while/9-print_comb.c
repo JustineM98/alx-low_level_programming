2decc805b3f5151af862aebd6eec7ad7ebf338cd
JustineM98 <justinemugishe98@gmail.com>
2022-03-15 22:34:23 +0200

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,37 +1,29 @@
+
 #include <stdio.h>
-
 /**
- * main - prints all possible combinations of single-digit numbers
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: Print all possible combinations fo single-digit numbers.
+ * Numbers must be separated by commas and a space.
+ * You can only use `putchar` to print to console
+ * You can only use `putchar` up to four times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
  */
-
 int main(void)
-
 {
+	int i = 0;
 
-int n;
-for (n = 48;
-n < 58;
-n++)
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar('\n');
 
-{
-
-putchar(n);
-
-if (n != 57)
-
-{
-
-putchar(',');
-
-putchar(' ');
-
-}
-
-}
-
-puitchar('\n');
-
-return (0);
-
+	return (0);
 }
