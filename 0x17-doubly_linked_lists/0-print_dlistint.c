95304b9ba225677ceeaff456e7ee92344e8bed6d
JustineM98 <justinemugishe98@gmail.com>
2022-06-13 14:44:06 +0200

    My project of 0x17-double_linked_lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -16,9 +16,9 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h)
 	{
-		printf("%d\n", h->n);
-		size++;
-		h = h->next;
+	printf("%d\n", h->n);
+	size++;
+	h = h->next;
 	}
 	return (size);
 }
