683d63e25edff6bebba5e908decc8c33a5cb0490
JustineM98 <justinemugishe98@gmail.com>
2022-06-13 14:55:37 +0200

    My project of 0x17-double_linked_lists

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -10,7 +10,7 @@
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next
- * Description: doubly linked list node structure
+ * Description: doubly linked list node structure for Holberton project
  */
 typedef struct dlistint_s
 {
@@ -29,4 +29,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif /* LISTS_H*/
+#endif
